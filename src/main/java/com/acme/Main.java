53c75e705344119e902b290ca0f991c5858e3340
cbmarcum <carl.marcum@codebuilders.net>
2022-11-20 08:01:37 -0500

    uncapitalized world

--- src/main/java/com/acme/Main.java
+++ src/main/java/com/acme/Main.java
@@ -5,7 +5,7 @@ package com.acme;
  */
 public class Main {
     public static void main(String[] args) {
-        String name = "World";
+        String name = "world";
         if (args != null && args.length > 0) name = args[0];
         System.out.println("Hello " + name);
     }
