e3ef8dc84f83260f37073835eee3fa4d26da6943
Carl Marcum <carl.marcum@codebuilders.net>
2022-12-29 08:21:00 -0500

    change World to Everyone

--- src/main/java/com/acme/Main.java
+++ src/main/java/com/acme/Main.java
@@ -1,11 +1,11 @@
 package com.acme;
 
 /**
- * Class to print Hello World
+ * Class to print Hello Everyone
  */
 public class Main {
     public static void main(String[] args) {
-        String name = "World";
+        String name = "Everyone";
         if (args != null && args.length > 0) name = args[0];
         System.out.println("Hello " + name);
     }
